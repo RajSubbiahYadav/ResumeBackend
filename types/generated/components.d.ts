e31f4026eddf0d8a24c54df80d08373223603a1b
RajSubbiahYadav <rajsubbiahyadav@gmail.com>
2024-12-08 22:40:05 +0530

    Inital Commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,70 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'education';
+    icon: 'file';
+  };
+  attributes: {
+    degree: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    endDate: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    universityName: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    icon: 'dashboard';
+  };
+  attributes: {
+    city: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    workSummery: Schema.Attribute.RichText;
+  };
+}
+
+export interface ProjectProject extends Struct.ComponentSchema {
+  collectionName: 'components_project_projects';
+  info: {
+    description: '';
+    displayName: 'project';
+    icon: 'grid';
+  };
+  attributes: {
+    description: Schema.Attribute.RichText;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'skills';
+    icon: 'walk';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'project.project': ProjectProject;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
